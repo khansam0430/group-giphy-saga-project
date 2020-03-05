4d9a5bfb167514faace7deb19ce187984b10cc4c
Samreen Khan <khansam0430@gmail.com>
2020-03-05 12:17:15 -0600

    made template of index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import './index.css';
+import App from './components/App/App.js';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from "redux-saga/effects";
+import Axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// Create the rootSaga generator function
+function* rootSaga() {
+ 
+
+}
+
+function* addGiphy(action) {
+
+}
+
+function* postGiphy() {
+  
+}
+
+function* deleteGiphy(action) {
+
+}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// This function (our reducer) will be called when an 
+// action is dipatched. state = ['Apple'] sets the default 
+// value of the array.
+const giphyReducer = (state = [], action) => {
+
+}
+
+// Create one store that all components can use
+const storeInstance = createStore(
+    combineReducers({
+        giphyReducer
+    }),
+    // Add sagaMiddleware to our store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+    document.getElementById('root'));
+
+
